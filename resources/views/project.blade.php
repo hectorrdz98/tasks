79179c9aca88fd87298083edc538e7966025596b
Héctor Rdz <hd.rdz98@gmail.com>
2020-11-19 10:48:45 -0600

    Project Blade Design

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -12,20 +12,44 @@
             </div>
         </div>
         <div class="p-4 pb-0 bg-blue-50 rounded-t-3xl shadow-2xl">
-            <p class="text-lg h-10 text-gray-800 font-semibold flex items-center justify-start">
-                To Do 
-                <span class="ml-2 text-gray-400">(30)<span>
-            </p>
-            <div class="mt-4 w-full overflow-y-auto" style="height: calc(100% - 1rem - 2.5rem - 1rem - 10rem - 3.5rem - 3rem);">
-                @for ($i = 0; $i < 5; $i++)
-                    @include('resources/taskCard')
-                @endfor
+            <div id="panel-to-do">
+                <p class="text-lg h-10 text-gray-800 font-semibold flex items-center justify-start">
+                    To Do 
+                    <span class="ml-2 text-gray-400">(30)<span>
+                </p>
+                <div class="mt-4 w-full overflow-y-auto" style="height: calc(100% - 1rem - 2.5rem - 1rem - 10rem - 3.5rem - 3rem);">
+                    @for ($i = 0; $i < 30; $i++)
+                        @include('resources/taskCard')
+                    @endfor
+                </div>
+            </div>
+            <div id="panel-doing" class="hidden">
+                <p class="text-lg h-10 text-gray-800 font-semibold flex items-center justify-start">
+                    Doing
+                    <span class="ml-2 text-gray-400">(8)<span>
+                </p>
+                <div class="mt-4 w-full overflow-y-auto" style="height: calc(100% - 1rem - 2.5rem - 1rem - 10rem - 3.5rem - 3rem);">
+                    @for ($i = 0; $i < 8; $i++)
+                        @include('resources/taskCard')
+                    @endfor
+                </div>
+            </div>
+            <div id="panel-done" class="hidden">
+                <p class="text-lg h-10 text-gray-800 font-semibold flex items-center justify-start">
+                    Done
+                    <span class="ml-2 text-gray-400">(2)<span>
+                </p>
+                <div class="mt-4 w-full overflow-y-auto" style="height: calc(100% - 1rem - 2.5rem - 1rem - 10rem - 3.5rem - 3rem);">
+                    @for ($i = 0; $i < 2; $i++)
+                        @include('resources/taskCard')
+                    @endfor
+                </div>
             </div>
             <div class="w-full h-10 mt-2 flex justify-center items-center">
                 <div class="w-full p-2 flex flex-col items-center justify-center">
-                    <div class="w-full bg-blue-500 flex justify-center items-center rounded">
-                        <i class="far fa-clock text-white"></i>
-                        <p class="text-base text-white font-semibold ml-2">
+                    <div id="option-to-do" class="project-options text-white w-full bg-blue-500 flex justify-center items-center rounded">
+                        <i class="far fa-clock"></i>
+                        <p class="text-base font-semibold ml-2">
                             To Do
                         </p>
                     </div>
@@ -34,9 +58,9 @@
                     <div class="w-full h-2/3 bg-gray-400"></div>
                 </div>
                 <div class="w-full p-2 flex flex-col items-center justify-center">
-                    <div class="w-full flex justify-center items-center rounded">
-                        <i class="fas fa-adjust text-gray-800"></i>
-                        <p class="text-base text-gray-800 font-semibold ml-2">
+                    <div id="option-doing" class="project-options text-gray-800 w-full flex justify-center items-center rounded">
+                        <i class="fas fa-adjust"></i>
+                        <p class="text-base font-semibold ml-2">
                             Doing
                         </p>
                     </div>
@@ -45,9 +69,9 @@
                     <div class="w-full h-2/3 bg-gray-400"></div>
                 </div>
                 <div class="w-full p-2 flex flex-col items-center justify-center">
-                    <div class="w-full flex justify-center items-center rounded">
-                        <i class="far fa-check-circle text-gray-800"></i>
-                        <p class="text-base text-gray-800 font-semibold ml-2">
+                    <div id="option-done" class="project-options text-gray-800 w-full flex justify-center items-center rounded">
+                        <i class="far fa-check-circle "></i>
+                        <p class="text-base font-semibold ml-2">
                             Done
                         </p>
                     </div>
@@ -55,4 +79,34 @@
             </div>
         </div>
     </div>
+@endsection
+@section('scripts')
+<script>
+    $(".project-options").click(function () {
+        $(".project-options").removeClass("bg-blue-500");
+        $(".project-options").removeClass("text-white");
+        $(".project-options").addClass("text-gray-800");
+        $(this).addClass("bg-blue-500");
+        $(this).removeClass("text-gray-800");
+        $(this).addClass("text-white");
+    });
+
+    $("#option-to-do").click(function () {
+        $("#panel-to-do").slideDown();
+        $("#panel-doing").slideUp();
+        $("#panel-done").slideUp();
+    });
+
+    $("#option-doing").click(function () {
+        $("#panel-to-do").slideUp();
+        $("#panel-doing").slideDown();
+        $("#panel-done").slideUp();
+    });
+
+    $("#option-done").click(function () {
+        $("#panel-to-do").slideUp();
+        $("#panel-doing").slideUp();
+        $("#panel-done").slideDown();
+    });
+</script>
 @endsection
